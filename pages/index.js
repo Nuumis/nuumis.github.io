2bd791865cf8747ccff8cdd4a686d06b260fd32a
Vili-Petteri Maunu <vilipetterimaunu@gmail.com>
2023-02-17 13:12:10 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className=' min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-burtons' >Vpm</h1>
+            <h1 className='text-xl font-burtons' >TOIMIIKS</h1>
             <ul className='flex items-center'>
               <li>
                 <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
