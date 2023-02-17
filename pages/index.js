622739b2f278cefa58af35f0779eaffe4d4a609f
Vili-Petteri Maunu <vilipetterimaunu@gmail.com>
2023-02-17 17:50:54 +0200

    fixes

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import web6 from '../public/web6.png'
 import { useState } from 'react'
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(true)
+  const [darkMode, setDarkMode] = useState(false)
   return (
     <div className={darkMode ? "dark" : ""}>
 
@@ -27,28 +27,27 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
 
         </Head>
-        <main className='bg-white px-10 md:px-20 lg:px-60 dark:bg-gray-900'>
+        <main className='bg-white px-10 md:px-20 lg:px-60 dark:bg-gray-900 font-rubik'>
           <section className=' min-h-screen'>
             <nav className='py-10 mb-12 flex justify-between'>
-              <h1 className='text-xl font-rubik' >nuumis.</h1>
+              <h1 className='text-xl font-rubik text-black dark:text-white'  >nuumis.</h1>
               <ul className='flex items-center'>
                 <li>
                   <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl' />
                 </li>
                 <li>
                   <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a>
-                  <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a>
 
                 </li>
               </ul>
             </nav>
             <div className='text-center p-10'>
               <h2 className='text-5xl py-2 font-rubik text-teal-600 font-medium md:text-6xl'>Vili-Petteri Maunu</h2>
-              <h3 className='text-2xl py-2 font-rubik md:text-3xl'>Developer and designer.</h3>
+              <h3 className='text-2xl py-2 font-rubik md:text-3xl text-black dark:text-gray-100'>ICT-engineering student</h3>
               <p className='text-md py-5 leading-9 font-rubik text-gray-600 md:text-xl'>Welcome to my website! I&apos;m an ICT engineering student with a passion for programming
                 and web development, and I&apos;m excited to share my journey and projects with you.</p>
             </div>
-            <div className='text-5xl py-8 flex justify-center gap-7 text-red-400'>
+            <div className='text-5xl py-8 flex justify-center gap-7 text-gray-500'>
               <AiFillInstagram />
               <AiFillLinkedin />
               <AiFillGithub />
@@ -59,62 +58,62 @@ export default function Home() {
           </section>
           <section>
             <div className='text-center'>
-              <h3 className='text-3xl py-1'>Placeholder</h3>
-              <p className='text-md py-5 leading-9 font-rubik text-gray-600'>Welcome to my website! I&apos;m an ICT engineering
+              <h3 className='text-3xl py-1 font-rubik'>Placeholder</h3>
+              <p className='text-md py-5 leading-9 font-rubik text-gray-600 md:text-xl'>Welcome to my website! I&apos;m an ICT engineering
                 student with a passion for programming and web development, and I&apos;m excited to
                 share my journey and projects with you.</p>
             </div>
             <div className='lg:flex gap-10'>
               <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
                 <Image src={asd1} alt="image" width={100} height={100} />
-                <h3 className="text-lg font-medium pt-8 pb-2  ">
-                  Beautiful Designs
+                <h3 className="text-lg font-rubik font-medium pt-8 pb-2  ">
+                  Awsum Designs
                 </h3>
-                <p className="py-2">
+                <p className="py-2 font-rubik">
                   Creating elegant designs suited for your needs following core
                   design theory.
                 </p>
                 <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
                 <p className="text-gray-800 py-1">Photoshop</p>
                 <p className="text-gray-800 py-1">Midjourney</p>
-                <p className="text-gray-800 py-1">Figma</p>
-                <p className="text-gray-800 py-1">Indesign</p>
+                <p className="text-gray-800 py-1">Lightroom</p>
+
               </div>
               <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
                 <Image src={asd2} alt="image" width={100} height={100} />
-                <h3 className="text-lg font-medium pt-8 pb-2  ">
-                  Beautiful Designs
+                <h3 className="text-lg font-rubik font-medium pt-8 pb-2  ">
+                  Awsum Designs
                 </h3>
-                <p className="py-2">
+                <p className="py-2 font-rubik">
                   Creating elegant designs suited for your needs following core
                   design theory.
                 </p>
                 <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
                 <p className="text-gray-800 py-1">Photoshop</p>
                 <p className="text-gray-800 py-1">Midjourney</p>
-                <p className="text-gray-800 py-1">Figma</p>
-                <p className="text-gray-800 py-1">Indesign</p>
+                <p className="text-gray-800 py-1">Lightroom</p>
+
               </div>
               <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
                 <Image src={asd3} alt="image" width={100} height={100} />
-                <h3 className="text-lg font-medium pt-8 pb-2  ">
-                  Beautiful Designs
+                <h3 className="text-lg font-rubik font-medium pt-8 pb-2  ">
+                  Awsum Designs
                 </h3>
-                <p className="py-2">
+                <p className="py-2 font-rubik">
                   Creating elegant designs suited for your needs following core
                   design theory.
                 </p>
                 <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
                 <p className="text-gray-800 py-1">Photoshop</p>
                 <p className="text-gray-800 py-1">Midjourney</p>
-                <p className="text-gray-800 py-1">Figma</p>
-                <p className="text-gray-800 py-1">Indesign</p>
+                <p className="text-gray-800 py-1">Lightroom</p>
+
               </div>
 
 
             </div>
             <div className='text-center'>
-              <h3>Portfolio header</h3>
+              <h3 >Portfolio header</h3>
               <p>Portfolio text</p>
             </div>
             <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
